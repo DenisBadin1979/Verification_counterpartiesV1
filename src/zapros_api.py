9bc0171a7c1b0ba996439fe66df67363ab926d27
Denis Badin <denisbadin@yandex.ru>
2025-08-19 19:47:46 +0500

    добавлено про запросы

--- src/zapros_api.py
+++ src/zapros_api.py
@@ -1,8 +1,12 @@
 # https://github.com/hflabs/dadata-py
 
 from dadata import Dadata
-token = "6524fe79cd1bc844f004d4076bbb5efb4f54355f"
-dadata = Dadata(token)
-result = dadata.find_by_id( "party","7726454328")
+def zapros_min (inn):
+    token = "6524fe79cd1bc844f004d4076bbb5efb4f54355f"
+    dadata = Dadata(token)
+    result = dadata.find_by_id( "party",inn)
+    return result
 
-print (result)
+
+
+print (i_inn)
